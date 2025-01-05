c9a9c24b9d6004638a5fe66be61cad051c34aa66
almost-good <marja.marinic@gmail.com>
2025-01-05 16:58:44 +0000

    add

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    
+    if request.method == 'POST':
+        return HttpResponse("You must have posted something.")
+    else:
+        return HttpResponse(request.method)
